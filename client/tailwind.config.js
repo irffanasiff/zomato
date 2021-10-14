e9d60bcd75d8c65ed2b697e36249a4a220f4cd33
Irfan Asif <hashtag.irfan@gmail.com>
2021-10-14 18:31:06 +0530

    🚀 added react icons

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,19 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        zomato: {
+          red: '#e23744',
+          pink: '#f47c8A',
+          yellow: '#f8d149',
+          blue: '#2781e7',
+          teal: '#009999',
+          coal: '#1c1c1c',
+          grey: '#4f4f4f',
+        },
+      },
+    },
   },
   variants: {
     extend: {},
