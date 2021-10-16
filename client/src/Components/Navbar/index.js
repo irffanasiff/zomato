c2a95fc83ab938679cdba62c3b4aa0abb3683bd6
Irfan Asif <hashtag.irfan@gmail.com>
2021-10-16 12:27:22 +0530

    🍕 added signin and signup modals

--- client/src/Components/Navbar/index.js
+++ client/src/Components/Navbar/index.js
@@ -3,14 +3,24 @@ import { FaUserAlt } from 'react-icons/fa';
 import { HiLocationMarker } from 'react-icons/hi';
 import { IoMdArrowDropdown, IoMdArrowDropup } from 'react-icons/io';
 import { RiSearch2Line } from 'react-icons/ri';
-import SignIn from '.././Auth/Signin';
-import UserDropdown from './UserDropdown';
+import { useSelector, useDispatch } from 'react-redux';
+import gravatar from 'gravatar';
 
-const MobileNav = ({ setOpenSignin }) => {
+// components
+import SignIn from '../Auth/SignIn';
+import SignUp from '../Auth/SignUp';
+
+// redux actions
+import { signOut } from '../../Redux/Reducer/Auth/Auth.action';
+
+const MobileNav = ({ SignIn, SignUp }) => {
   const [isDropDownOpen, setIsDropDownOpen] = useState(false);
-  const toggleDropDown = () => {
-    setIsDropDownOpen(true);
-  };
+  const dispatch = useDispatch();
+
+  const reduxState = useSelector((global) => global.user.user);
+
+  const signOutHandler = () => dispatch(signOut());
+
   return (
     <div className='flex w-full items-center justify-between lg:hidden'>
       <div className='w-28'>
@@ -24,24 +34,52 @@ const MobileNav = ({ setOpenSignin }) => {
         <button className='bg-zomato-400 text-white py-2 px-3 rounded-full'>
           Use App
         </button>
-        <span
-          onClick={toggleDropDown}
-          className='border p-2 border-gray-300 text-zomato-400 rounded-full'
-        >
-          <FaUserAlt />
-        </span>
-        {isDropDownOpen && (
-          <div className='absolute -bottom-16 -right-4 w-full z-20 flex flex-col gap-2'>
-            <button>Sign In</button>
-            <button>Sign Up</button>
-          </div>
+        {reduxState?.user?.fullname ? (
+          <>
+            {' '}
+            <div
+              onClick={() => setIsDropDownOpen((prev) => !prev)}
+              className='border p-2 border-gray-300 text-zomato-400 w-20 h-20 rounded-full'
+            >
+              <img
+                src={gravatar.url(reduxState?.user?.email)}
+                alt={reduxState?.user?.email}
+                className='w-full h-full rounded-full object-cover'
+              />
+            </div>
+            {isDropDownOpen && (
+              <div className='absolute shadow-lg py-3 -bottom-20 -right-4 w-full bg-white z-20 flex flex-col gap-2'>
+                <button onClick={signOutHandler}>Sign Out</button>
+              </div>
+            )}
+          </>
+        ) : (
+          <>
+            <span
+              onClick={() => setIsDropDownOpen((prev) => !prev)}
+              className='border p-2 border-gray-300 text-zomato-400 rounded-full'
+            >
+              <FaUserAlt />
+            </span>
+            {isDropDownOpen && (
+              <div className='absolute shadow-lg py-3 -bottom-20 -right-4 w-full bg-white z-20 flex flex-col gap-2'>
+                <button onClick={SignIn}>Sign In</button>
+                <button onClick={SignUp}>Sign Up</button>
+              </div>
+            )}
+          </>
         )}
       </div>
     </div>
   );
 };
 
-const LargeNav = () => {
+const LargeNav = ({ SignIn, SignUp }) => {
+  const [isDropDownOpen, setIsDropDownOpen] = useState(false);
+  const dispatch = useDispatch();
+  const reduxState = useSelector((global) => global.user.user);
+  const signOutHandler = () => dispatch(signOut());
+
   return (
     <>
       <div className='hidden lg:inline container px-20 mx-auto'>
@@ -74,14 +112,41 @@ const LargeNav = () => {
               />
             </div>
           </div>
-          <div className='ml-28 flex gap-4 '>
-            <button className='text-gray-500 text-xl hover:text-gray-800'>
-              Login
-            </button>
-            <button className='text-gray-500 text-xl hover:text-gray-800'>
-              Signup
-            </button>
-          </div>
+          {reduxState?.user?.fullname ? (
+            <div className='relative w-20'>
+              {' '}
+              <div
+                onClick={() => setIsDropDownOpen((prev) => !prev)}
+                className='border p-2 border-gray-300 text-zomato-400 w-full h-20 rounded-full'
+              >
+                <img
+                  src={gravatar.url(reduxState?.user?.email)}
+                  alt={reduxState?.user?.email}
+                  className='w-full h-full rounded-full object-cover'
+                />
+              </div>
+              {isDropDownOpen && (
+                <div className='absolute shadow-lg py-3  -right-4 w-full bg-white z-30 flex flex-col gap-2'>
+                  <button onClick={signOutHandler}>Sign Out</button>
+                </div>
+              )}
+            </div>
+          ) : (
+            <div className='ml-28 flex gap-4 '>
+              <button
+                onClick={SignIn}
+                className='text-gray-500 text-xl hover:text-gray-800'
+              >
+                Login
+              </button>
+              <button
+                onClick={SignUp}
+                className='text-gray-500 text-xl hover:text-gray-800'
+              >
+                Signup
+              </button>
+            </div>
+          )}
         </div>
       </div>
     </>
@@ -92,12 +157,16 @@ const Navbar = () => {
   const [openSignin, setOpenSignin] = useState(false);
   const [openSignup, setOpenSignup] = useState(false);
 
+  const openSignInmodal = () => setOpenSignin(true);
+  const openSignUpmodal = () => setOpenSignup(true);
   return (
     <>
       <SignIn isOpen={openSignin} setIsOpen={setOpenSignin} />
+      <SignUp isOpen={openSignup} setIsOpen={setOpenSignup} />
+
       <nav className='p-4 flex bg-white shadow-md lg:shadow-none w-full items-center'>
-        <MobileNav SignIn={{ openSignin, setOpenSignin }} />
-        <LargeNav SignIn={{ openSignin, setOpenSignin }} />
+        <MobileNav SignIn={openSignInmodal} SignUp={openSignUpmodal} />
+        <LargeNav SignIn={openSignInmodal} SignUp={openSignUpmodal} />
       </nav>
     </>
   );
